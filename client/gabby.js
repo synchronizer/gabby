aa884cd6e73dbe0294a559d365d877bc00a9739e
synchronizer <synchronizer@yandex.ru>
2026-08-31 18:51:25 +0300

    Restore Node client

--- client/gabby.js
+++ client/gabby.js
@@ -1,8 +1,6 @@
-console.log('GABBY LOADED')
-
 const apiUrl =
     document.currentScript.dataset.gabbyApi ||
-    'https://gabby-test.synchronizer.workers.dev'
+    'https://gabby-zw4z.onrender.com'
 
 
 document.addEventListener('submit', async (event) => {
